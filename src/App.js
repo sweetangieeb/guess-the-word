1b84563f8ed295dfd1fa6086e74944c81c97048c
Angela Brown <angelabrown@Angelas-MacBook-Pro.local>
2022-06-23 01:41:59 -0400

    added components folder

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
             Hint
           </h5>
         </div>
-        
+
       </div>
     </div>
   );
---
+++ src/components/Title/index.js
@@ -0,0 +1,5 @@
+export function Title() {
+    return (
+      <div></div>
+    );
+  };
